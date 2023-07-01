278950604a5e0ff3429c506ad945f799a0b98b61
Christian <c98e@outlook.de>
2023-07-02 00:54:12 +0200

    point counter

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,29 @@ import cv2
 import numpy as np
 import requests
 import base64
+import pandas as pd
 
-#rf = Roboflow(api_key=st.secrets["RF_API_KEY"])
-#project = rf.workspace().project("sticky-dot-counter")
-#model = project.version(1).model
-
-ROBOFLOW_MODEL = "sticky-dot-counter"
+ROBOFLOW_MODEL = st.secrets["RF_MODEL"]
 ROBOFLOW_API_KEY = st.secrets["RF_API_KEY"]
 
 upload_url = f"https://detect.roboflow.com/{ROBOFLOW_MODEL}/1?api_key={ROBOFLOW_API_KEY}"
 
+CLASS_TO_COLOR = {
+    "red-dot": (255, 0, 0),
+    "yellow-dot": (255, 255, 0),
+    "green-dot": (0, 255, 0)
+}
+
+CLASS_TO_VALUE = {
+    "red-dot": 0,
+    "yellow-dot": 1,
+    "green-dot": 2
+}
 
 img_file_buffer = st.camera_input("Take a picture")
+
+st.write("Detektion...")
+FRAME_WINDOW = st.image([])
 
 if img_file_buffer is not None:
     # To read image file buffer with OpenCV:
@@ -29,24 +40,29 @@ if img_file_buffer is not None:
         data=img_str, 
         headers={"Content-Type": "application/x-www-form-urlencoded"})
 
-    st.write(resp.json())
+    if resp.status_code == 200:
+        detection = resp.json()
+        rgb_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+        
 
-    st.write(type(cv2_img))
+        data_y = [i["y"] for i in detection["predictions"]]
+        data_class = [CLASS_TO_VALUE[i["class"]] for i in detection["predictions"]]
+        d = {"y": data_y, "class": data_class}
+        df = pd.DataFrame(data=d)
+        df = df.sort_values(by=["y"])
+        st.write(f"Anzahl Detektionen {df.shape[0]}")
+        punkte_3fach = df["class"].iloc[:-5].sum()
+        punkte_1fach = df["class"].iloc[-5:].sum()
+        punkte_ges = punkte_3fach*3 + punkte_1fach
+        st.write(f"Punktzahl: {punkte_ges}")
+        
+        st.write(detection)
+        for pred in detection["predictions"]:
+            x0 = int(pred["x"] - (pred["width"]/2))
+            y0 = int(pred["y"] - (pred["height"]/2))
+            x1 = int(pred["x"] + (pred["width"]/2))
+            y1 = int(pred["y"] + (pred["height"]/2))
 
- #   st.write(model.predict(img_str, confidence=40, overlap=30).json())
+            rgb_img = cv2.rectangle(rgb_img, (x0, y0), (x1, y1), CLASS_TO_COLOR[pred["class"]], 2)
 
-    # Check the type of cv2_img:
-    # Should output: <class 'numpy.ndarray'>
-    st.write(type(cv2_img))
-
-    # Check the shape of cv2_img:
-    # Should output shape: (height, width, channels)
-    st.write(cv2_img.shape)
-
-
-
-st.markdown(
-    "This demo uses a model and code from "
-    "https://github.com/robmarkcole/object-detection-app. "
-    "Many thanks to the project."
-)
+        FRAME_WINDOW.image(rgb_img)
