6a86dce4b967e12e956eb658f267b1190b6e201c
Christian <c98e@outlook.de>
2023-07-03 19:31:04 +0200

    cashing

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,13 @@ import numpy as np
 import requests
 import base64
 import pandas as pd
+from datetime import datetime
 
 ROBOFLOW_MODEL = st.secrets["RF_MODEL"]
 ROBOFLOW_API_KEY = st.secrets["RF_API_KEY"]
 
 if 'liste' not in st.session_state:
     st.session_state.liste = []
-
-upload_url = f"https://detect.roboflow.com/{ROBOFLOW_MODEL}/1?api_key={ROBOFLOW_API_KEY}"
 
 CLASS_TO_COLOR = {
     "red-dot": (255, 0, 0),
@@ -32,34 +31,27 @@ ROW_NAMES = [
     "Zimmer"
 ]
 
-img_file_buffer = st.camera_input("Take a picture", key="camera")
-
-FRAME_WINDOW = st.image([])
-
-id = 0
-pt_ges = 0
-
-if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-    rgb_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    
-    # ARUCO
-    aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+# FUNCTIONS
+@st.cache
+def detect_aruco_marker(img):
+    id = 0
+    aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_250)
     parameters = aruco.DetectorParameters()
     detector = aruco.ArucoDetector(aruco_dict, parameters)
 
-    gray = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     corners, ids, rejected = detector.detectMarkers(gray)
     if ids is not None:
         aruco.drawDetectedMarkers(rgb_img, corners, ids)
         if len(ids) == 1:
             id = ids[0][0]
+    return id
 
-    # Point Detection
-    retval, buffer = cv2.imencode('.jpg', cv2_img)
+@st.cache
+def detect_points(img):
+    retval, buffer = cv2.imencode('.jpg', img)
     img_str = base64.b64encode(buffer)
+    upload_url = f"https://detect.roboflow.com/{ROBOFLOW_MODEL}/1?api_key={ROBOFLOW_API_KEY}"
 
     resp = requests.post(
         upload_url, 
@@ -78,66 +70,92 @@ if img_file_buffer is not None:
             "class": [i["class"] for i in detection["predictions"]],
             "points": [CLASS_TO_VALUE[i["class"]] for i in detection["predictions"]]
         }
+        return d
+
+@st.cache
+def draw_rows(df, img):
+    pt_sums = dict.fromkeys(ROW_NAMES, 0)
+    for i, row_name in enumerate(ROW_NAMES):
+        df_row = df.iloc[i*5:(i+1)*5]
+        if not df_row.empty:
+            points = int(df_row["points"].sum())
+            pt_sums[row_name] = points
+            
+            min_x = int(df_row["x"].min() - df_row["w"].min()/2)
+            min_y = int(df_row["y"].min() - df_row["h"].min()/2)
+            max_x = int(df_row["x"].max() + df_row["w"].min()/2)
+            max_y = int(df_row["y"].max() + df_row["h"].min()/2)
+
+            cv2.rectangle(img, (min_x, min_y), (max_x, max_y), (0, 0, 255), 2)
+            cv2.putText(img, row_name, (max_x + 5, max_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+    return pt_sums
+
+@st.cache
+def draw_points(img):
+    for idx in df.index:
+        x0 = int(df["x"][idx] - (df["w"][idx]/2))
+        y0 = int(df["y"][idx] - (df["h"][idx]/2))
+        x1 = int(df["x"][idx] + (df["w"][idx]/2))
+        y1 = int(df["y"][idx] + (df["h"][idx]/2))
+        cv2.rectangle(img, (x0, y0), (x1, y1), CLASS_TO_COLOR[df["class"][idx]], 2)
+        cv2.putText(img, f"{int(df['conf'][idx]*100)}%", (x0 + 1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+
+@st.cache
+def generate_csv_from_list(dictionary):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    df = pd.DataFrame(dictionary)
+    return df.to_csv().encode('utf-8')
+
+# WEBSITE
+
+img_file_buffer = st.camera_input("Take a picture", key="camera")
+
+FRAME_WINDOW = st.image([])
+
+id = 0
+pt_ges = 0
+
+if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    rgb_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+    
+    # ARUCO
+    id = detect_aruco_marker(rgb_img)
+
+    # Point Detection
+    d = detect_points(cv2_img)
+    if d:
         df = pd.DataFrame(data=d)
         df = df.sort_values(by=["y"])
 
-        # Reihenerkennung
-        pt_sums = dict.fromkeys(ROW_NAMES, 0)
-        for i, row_name in enumerate(ROW_NAMES):
-            df_row = df.iloc[i*5:(i+1)*5]
-            if not df_row.empty:
-                points = int(df_row["points"].sum())
-                min_x = int(df_row["x"].min() - df_row["w"].min()/2)
-                min_y = int(df_row["y"].min() - df_row["h"].min()/2)
-                max_x = int(df_row["x"].max() + df_row["w"].min()/2)
-                max_y = int(df_row["y"].max() + df_row["h"].min()/2)
-
-                pt_sums[row_name] = points
-
-                rgb_img = cv2.rectangle(rgb_img, (min_x, min_y), (max_x, max_y), (0, 0, 255), 2)
-                rgb_img = cv2.putText(rgb_img, row_name, (max_x + 5, max_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-
-        # Punkteerkennung
-        for idx in df.index:
-            x0 = int(df["x"][idx] - (df["w"][idx]/2))
-            y0 = int(df["y"][idx] - (df["h"][idx]/2))
-            x1 = int(df["x"][idx] + (df["w"][idx]/2))
-            y1 = int(df["y"][idx] + (df["h"][idx]/2))
-            rgb_img = cv2.rectangle(rgb_img, (x0, y0), (x1, y1), CLASS_TO_COLOR[df["class"][idx]], 2)
-            rgb_img = cv2.putText(rgb_img, f"{int(df['conf'][idx]*100)}%", (x0 + 1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
-            
+        pt_sums = draw_rows(df, rgb_img) # Reihenerkennung
+        draw_points(rgb_img) # Punkteerkennung
         FRAME_WINDOW.image(rgb_img)
         
         pt_ges = pt_sums["Vers"] * 3 + pt_sums["Mitarbeit"] * 3 + pt_sums["Benehmen"] * 3 + pt_sums["Zimmer"]
         
         st.write(f"{df.shape[0]} von 20 Punkten wurden erkannt...")
-        #col1, col2 = st.columns(2)
-        #with col1:
-        #    st.markdown(f"""
-        #    Vers:  
-        #    Mitarbeit:  
-        #    Benehmen:  
-        #    Zimmer:
-        #    #### Gesamtpunkte:
-        #    """)
-        
-        #with col2:
-        #    st.markdown(f"""
-        #    &emsp; **{pt_sums["Vers"]}** * 3  
-        #    &plus;&ensp; **{pt_sums["Mitarbeit"]}** * 3  
-        #    &plus;&ensp;  **{pt_sums["Benehmen"]}** * 3  
-        #    &plus;&ensp;  **{pt_sums["Zimmer"]}** * 1
-        #    #### =&ensp; {pt_ges}
-        #    """)
+
         with st.expander(f"Gesamtpunktzahl: {pt_ges} (siehe Erläuterung)"):
-            st.markdown(f"""
-                | Kategorie    | Punkte     |
-                |--------------|------------|
-                | Vers         | &emsp; **{pt_sums["Vers"]}** * 3 |
-                | Mitarbeit    | &plus;&ensp; **{pt_sums["Mitarbeit"]}** * 3 |
-                | Benehmen     | &plus;&ensp;  **{pt_sums["Benehmen"]}** * 3 |
-                | Zimmer       | &plus;&ensp;  **{pt_sums["Zimmer"]}** * 1 |
-                | **Gesamtpunkte** | =&ensp; **{pt_ges}** |
+            col1, col2 = st.columns(2)
+            with col1:
+                st.markdown(f"""
+                Vers:  
+                Mitarbeit:  
+                Benehmen:  
+                Zimmer:
+                #### Gesamtpunkte:
+                """)
+            
+            with col2:
+                st.markdown(f"""
+                &emsp; **{pt_sums["Vers"]}** * 3  
+                &plus;&ensp; **{pt_sums["Mitarbeit"]}** * 3  
+                &plus;&ensp;  **{pt_sums["Benehmen"]}** * 3  
+                &plus;&ensp;  **{pt_sums["Zimmer"]}** * 1
+                #### =&ensp; {pt_ges}
                 """)
 
 
@@ -165,8 +183,13 @@ with but_col3:
     if st.button('Clear all detections'):
         st.session_state.liste = []
 
+csv = generate_csv_from_list(st.session_state.liste)
 with but_col4:
-    if st.button('Commit list'):
-        st.write("Not working")
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name=f'punkte_{datetime.now().strftime("%Y%m%d-%H%M%S")}.csv',
+        mime='text/csv',
+    )
 
 st.table(st.session_state.liste)
