87af653888a02e490dadb1d884e230a947575d8c
Christian Eckert <76399042+c-eckert@users.noreply.github.com>
2023-07-09 08:51:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,9 +36,9 @@ st.write('''<style>
     flex: 1 1 calc(33.3333% - 1rem) !important;
     min-width: calc(33% - 1rem) !important;
 }
+</style>
+<style>
 [data-testid="button"] {
-    width: calc(25% - 1rem) !important;
-    flex: 1 1 calc(25% - 1rem) !important;
     min-width: calc(25% - 1rem) !important;
 }
 </style>''', unsafe_allow_html=True)
