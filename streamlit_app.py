cf03a59ffa52784c2bf527449af37bbd6c6e4f4f
Christian Eckert <76399042+c-eckert@users.noreply.github.com>
2023-07-09 08:06:26 +0200

    Update column size streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,15 @@ ROW_NAMES = [
     "Zimmer"
 ]
 
+st.write('''<style>
+
+[data-testid="column"] {
+    width: calc(33.3333% - 1rem) !important;
+    flex: 1 1 calc(33.3333% - 1rem) !important;
+    min-width: calc(33% - 1rem) !important;
+}
+</style>''', unsafe_allow_html=True)
+
 # FUNCTIONS
 @st.cache_data
 def detect_aruco_marker(img):
